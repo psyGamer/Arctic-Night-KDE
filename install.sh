622b3b79f862b09d197543dab7136b3fcb86bcdd
psyGamer <psygamer21@gmail.com>
2022-04-07 20:09:13 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+cp Arctic-Night-Aurorae ~/.local/share/aurorae/Arctic-Night
+printf "\033[0;32mSuccessfully installed Aurorae theme\n"
+cp Arctic-Night-Kvantum ~/.config/Kvantum/themes/Arctic-Night
+printf "\033[0;32mSuccessfully installed Kvantum theme\n"
+
+printf "\n\033[1;32mDone!\033[0m\n"
