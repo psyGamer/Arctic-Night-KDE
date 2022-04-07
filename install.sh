56a2492ed70a0f65358cf2b36f02a697614674d5
psyGamer <psygamer21@gmail.com>
2022-04-07 21:53:21 +0200

    Fixed Kvantum install location

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp -r Arctic-Night-Aurorae ~/.local/share/aurorae/Arctic-Night
 printf "\033[0;32mSuccessfully installed Aurorae theme\n"
 
 mkdir -p ~/.config/Kvantum/themes/Arctic-Night
-cp -r Arctic-Night-Kvantum ~/.config/Kvantum/themes/Arctic-Night
+cp -r Arctic-Night-Kvantum ~/.config/Kvantum/Arctic-Night
 printf "\033[0;32mSuccessfully installed Kvantum theme\n"
 
 chmod +x ./uninstall.sh
