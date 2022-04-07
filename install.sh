8852dc7bbbadf3dd783e3f6448741f7f9e7019c7
psyGamer <psygamer21@gmail.com>
2022-04-07 21:45:01 +0200

    Fixed install script (again)

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
-mkdir -r ~/.local/share/aurorae/Arctic-Night
-cp Arctic-Night-Aurorae ~/.local/share/aurorae/Arctic-Night
+mkdir ~/.local/share/aurorae/Arctic-Night
+cp -r Arctic-Night-Aurorae ~/.local/share/aurorae/Arctic-Night
 printf "\033[0;32mSuccessfully installed Aurorae theme\n"
 
-mkdir -r ~/.config/Kvantum/themes/Arctic-Night
-cp Arctic-Night-Kvantum ~/.config/Kvantum/themes/Arctic-Night
+mkdir ~/.config/Kvantum/themes/Arctic-Night
+cp -r Arctic-Night-Kvantum ~/.config/Kvantum/themes/Arctic-Night
 printf "\033[0;32mSuccessfully installed Kvantum theme\n"
 
 chmod +x ./uninstall.sh
