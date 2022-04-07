302857582789c6936d10b9e6be480b292a989a55
psyGamer <psygamer21@gmail.com>
2022-04-07 21:58:20 +0200

    Simplified mkdir command

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-mkdir -p ~/.local/share/aurorae/themes/Arctic-Night
+mkdir -p ~/.local/share/aurorae/themes
 cp -r Arctic-Night-Aurorae ~/.local/share/aurorae/Arctic-Night
 printf "\033[0;32mSuccessfully installed Aurorae theme\n"
 
-mkdir -p ~/.config/Kvantum/themes/Arctic-Night
+mkdir -p ~/.config/Kvantum/themes
 cp -r Arctic-Night-Kvantum ~/.config/Kvantum/Arctic-Night
 printf "\033[0;32mSuccessfully installed Kvantum theme\n"
 
